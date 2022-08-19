ac3538e3462e6ff0a8a78a625044c288f9af4460
Valerio <darkseal@gmail.com>
2022-08-19 03:59:55 +0200

    Chapter 10, page 13

---
+++ Chapter_10/MyBGList/GraphQL/Query.cs
@@ -0,0 +1,31 @@
+﻿using MyBGList.Models;
+
+namespace MyBGList.GraphQL
+{
+    public class Query
+    {
+        [Serial]
+        [UsePaging]
+        [UseFiltering]
+        [UseSorting]
+        public IQueryable<BoardGame> GetBoardGames(
+            [Service] ApplicationDbContext context)
+            => context.BoardGames;
+
+        [Serial]
+        [UsePaging]
+        [UseFiltering]
+        [UseSorting]
+        public IQueryable<Domain> GetDomains(
+            [Service] ApplicationDbContext context)
+            => context.Domains;
+
+        [Serial]
+        [UsePaging]
+        [UseFiltering]
+        [UseSorting]
+        public IQueryable<Mechanic> GetMechanics(
+            [Service] ApplicationDbContext context)
+            => context.Mechanics;
+    }
+}
